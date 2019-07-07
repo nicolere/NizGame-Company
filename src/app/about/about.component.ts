0dc5e2ac0c372516d2fec8a646fe7c130e0a1e07
nicolere <nicolas.gauss.1@orange.fr>
2019-07-07 18:08:24 +0200

    dark mode toggle finished

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as $ from 'jquery';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
@@ -10,6 +10,15 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $(document).ready(function () {
+      $('.toggle').click(function () {
+        $('.toggle').toggleClass('active');
+        $('body').toggleClass('night');
+        $('.divider, .center.promo').toggleClass('night');
+        $('#light, #night').toggleClass('night');
+      })
+    })
+
   }
 
 }
