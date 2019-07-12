392f34e5855e8b670be7e7895c3d8993b3284d9b
nicolere <nicolas.gauss.1@orange.fr>
2019-07-12 16:32:16 +0200

    about appearance animation

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -8,6 +8,7 @@ import * as $ from 'jquery';
 })
 export class AboutComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit() {
@@ -21,6 +22,28 @@ export class AboutComponent implements OnInit {
         $('#modal-contact').toggleClass('night');
         $('span.character-counter').toggleClass('night');
       })
+
+      const ratio = 0.1;
+      const options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: ratio
+      }
+
+      const handleIntersect = function (entries, observer) {
+        entries.forEach(entry => {
+          if (entry.intersectionRatio > ratio) {
+            entry.target.classList.add('reveal-visible');
+            observer.unobserve(entry.target);
+          }
+        });
+      }
+
+      const observer = new IntersectionObserver(handleIntersect, options)
+      document.querySelectorAll('.reveal').forEach(function (r) {
+        observer.observe(r);
+      })
+
     })
 
   }
