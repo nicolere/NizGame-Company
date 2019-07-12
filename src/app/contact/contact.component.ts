e2cc82d509745e08744b6384e2f6fefade68db63
nicolere <nicolas.gauss.1@orange.fr>
2019-07-12 16:40:35 +0200

    contact appearance animation

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -18,6 +18,27 @@ export class ContactComponent implements OnInit {
 
     });
 
+    const ratio = 0.1;
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: ratio
+    }
+
+    const handleIntersect = function (entries, observer) {
+      entries.forEach(entry => {
+        if (entry.intersectionRatio > ratio) {
+          entry.target.classList.add('reveal-visible');
+          observer.unobserve(entry.target);
+        }
+      });
+    }
+
+    const observer = new IntersectionObserver(handleIntersect, options)
+    document.querySelectorAll('.reveal').forEach(function (r) {
+      observer.observe(r);
+    })
+
   }
 
 }
