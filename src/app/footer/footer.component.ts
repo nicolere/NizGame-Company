274a150557bb3fcc203e4884752df0c99cc0f334
nicolere <nicolas.gauss.1@orange.fr>
2019-07-16 23:43:35 +0200

    footer card + animation

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-footer',
@@ -10,6 +12,35 @@ export class FooterComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $(document).ready(function () {
+
+      var cards = document.querySelectorAll('.card');
+      cards.forEach(element => element.addEventListener('click', function () {
+        element.classList.toggle('is-flipped');
+      }))
+
+      const ratio = 0.1;
+      const options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: ratio
+      }
+
+      const handleIntersect = function (entries, observer) {
+        entries.forEach(entry => {
+          if (entry.intersectionRatio > ratio) {
+            entry.target.classList.add('reveal-visible');
+            observer.unobserve(entry.target);
+          }
+        });
+      }
+
+      const observer = new IntersectionObserver(handleIntersect, options)
+      document.querySelectorAll('.reveal').forEach(function (r) {
+        observer.observe(r);
+      })
+
+    })
   }
 
 }
