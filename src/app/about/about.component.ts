b4483cf551a813ea4ccbb55fd9e5e0a84343115c
Nicolas <nicolas.gauss.1@orange.fr>
2019-08-08 14:25:40 +0200

    trigger new class component

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,48 +1,48 @@
-import { Component, OnInit } from '@angular/core';
-import * as $ from 'jquery';
+import { Component, OnInit } from "@angular/core";
+import * as $ from "jquery";
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  selector: "app-about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.css"]
 })
 export class AboutComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
     $(document).ready(function() {
-      $('.toggle').click(function() {
-        $('.toggle').toggleClass('active');
-        $('body').toggleClass('night');
-        $('.divider, .center.promo').toggleClass('night');
-        $('#light, #night').toggleClass('night');
-        $('#contact .row, #contact #right .a').toggleClass('night');
-        $('#modal-contact').toggleClass('night');
-        $('span.character-counter').toggleClass('night');
-        $('.social-btns').toggleClass('night');
-        $('a#disclaimer, a#privacy').toggleClass('night');
-        $('#studios .card').toggleClass('night');
-        $('#left-p').toggleClass('night');
+      $(".toggle").click(function() {
+        $(".toggle").toggleClass("active");
+        $("body").toggleClass("night");
+        $(".divider, .center.promo").toggleClass("night");
+        $("#light, #night").toggleClass("night");
+        $("#contact .row, #contact #right .a").toggleClass("night");
+        $("#modal-contact").toggleClass("night");
+        $("span.character-counter").toggleClass("night");
+        $(".social-btns").toggleClass("night");
+        $("a#disclaimer, a#privacy").toggleClass("night");
+        $("#studios .card").toggleClass("night");
+        $("#left-p, #right-p").toggleClass("night");
       });
 
       const ratio = 0.1;
       const options = {
         root: null,
-        rootMargin: '0px',
+        rootMargin: "0px",
         threshold: ratio
       };
 
       const handleIntersect = function(entries, observer) {
         entries.forEach(entry => {
           if (entry.intersectionRatio > ratio) {
-            entry.target.classList.add('reveal-visible');
+            entry.target.classList.add("reveal-visible");
             observer.unobserve(entry.target);
           }
         });
       };
 
       const observer = new IntersectionObserver(handleIntersect, options);
-      document.querySelectorAll('.reveal').forEach(function(r) {
+      document.querySelectorAll(".reveal").forEach(function(r) {
         observer.observe(r);
       });
     });
